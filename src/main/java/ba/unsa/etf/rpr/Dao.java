151fbf38d01f1e841137f8ba99f53bda5f71de30
smuratovic3 <smuratovic3@etf.unsa.ba>
2022-12-05 14:27:58 +0100

    Made Dao Layer

---
+++ src/main/java/ba/unsa/etf/rpr/Dao.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr;
+
+import java.util.List;
+
+/**
+ * Root interface for all DAO classes
+ * @author Semina Muratovic
+ */
+
+public interface Dao<T> {
+
+
+    T getById(int id);
+
+
+    T add(T item);
+
+
+    T update(T item);
+
+
+    void delete(int id);
+
+
+    List<T> getAll();
+}
