72004a8bb95e6e86c6884756465ef09d76fa49c0
smuratovic3 <smuratovic3@etf.unsa.ba>
2022-12-11 14:43:27 +0100

    Added atributes,setters and getters, toString method and equals method in class User

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -1,4 +1,78 @@
 package ba.unsa.etf.rpr.domain;
+import java.util.Objects;
 
+/**
+ * Information about users
+ * @author Semina Muratović
+ **/
 public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String address;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    @Override
+    public String toString() {
+        return "User - " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname=" + surname +
+                ", email=" + email +
+                ", address=" + address +
+                '.';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, email,address);
+    }
 }
