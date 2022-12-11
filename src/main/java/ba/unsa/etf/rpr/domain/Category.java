83b2a587ae10ff05d54c8de71862a3f3d06007bf
smuratovic3 <smuratovic3@etf.unsa.ba>
2022-12-11 13:54:49 +0100

    Added atributes,setters and getters, toString method and equals method in class Category

--- src/main/java/ba/unsa/etf/rpr/domain/Category.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Category.java
@@ -1,4 +1,48 @@
 package ba.unsa.etf.rpr.domain;
+import java.util.Objects;
 
+/**
+ * List of possible categories for medicines
+ *
+ * @author Semina Muratović
+ */
 public class Category {
+    private int id;
+    private String name;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
