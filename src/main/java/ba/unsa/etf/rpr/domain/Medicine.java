aa6cc421906d9abd1cf620805bd85b366851ad57
smuratovic3 <smuratovic3@etf.unsa.ba>
2022-12-11 14:20:42 +0100

    Added atributes,setters and getters, toString method and equals method in class Medicine

--- src/main/java/ba/unsa/etf/rpr/domain/Medicine.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Medicine.java
@@ -1,4 +1,97 @@
 package ba.unsa.etf.rpr.domain;
+import java.util.Objects;
 
+/**
+ * Information about medicines
+ * @author Semina Muratović
+ **/
 public class Medicine {
+    private int id;
+    private String name;
+    private int price;
+    private int quantity;
+    private String description;
+    private int prescription;
+    private Category category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(int prescription) {
+        this.prescription = prescription;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    @Override
+    public String toString() {
+        return "Medicine - " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", description=" + description +
+                ", prescription=" + prescription +
+                ", category=" + category +
+                '.';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Medicine quote = (Medicine) o;
+        return id == quote.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, quantity, description,prescription,category);
+    }
 }
