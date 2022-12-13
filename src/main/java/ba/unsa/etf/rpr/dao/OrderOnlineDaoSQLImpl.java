d1774d393a2a51239f48f91ff1354e482d29a8f0
smuratovic3 <smuratovic3@etf.unsa.ba>
2022-12-13 13:13:05 +0100

    Hidden database data

--- src/main/java/ba/unsa/etf/rpr/dao/OrderOnlineDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OrderOnlineDaoSQLImpl.java
@@ -1,15 +1,25 @@
 package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.OrderOnline;
+import java.io.FileReader;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
 public class OrderOnlineDaoSQLImpl implements OrderOnlineDao {
 
     private Connection connection;
 
     public OrderOnlineDaoSQLImpl(){
         try{
-            this.connection = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7582920", "sql7582920", "aymkDbW2ny");
+            FileReader reader = new FileReader("src/main/resources/database.properties");
+            Properties p = new Properties();
+            p.load(reader);
+            String s1 = p.getProperty("url");
+            String s2 = p.getProperty("user");
+            String s3 = p.getProperty("password");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(s1,s2,s3);
         }catch (Exception e){
             e.printStackTrace();
         }
