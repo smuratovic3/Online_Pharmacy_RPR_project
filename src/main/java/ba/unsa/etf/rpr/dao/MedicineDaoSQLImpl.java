9b1919da3f4a6d7b09027553df03cb24d62347e4
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-12 17:18:14 +0100

    Formatting MedicineDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/MedicineDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MedicineDaoSQLImpl.java
@@ -3,7 +3,6 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Category;
 import ba.unsa.etf.rpr.domain.Medicine;
 import ba.unsa.etf.rpr.exceptions.MedicineException;
-
 import java.sql.ResultSet;
 import java.util.List;
 import java.util.Map;
@@ -17,29 +16,33 @@ import java.util.TreeMap;
 public class MedicineDaoSQLImpl extends AbstractDao<Medicine> implements MedicineDao {
 
     private static MedicineDaoSQLImpl instance = null;
-    private MedicineDaoSQLImpl() {
+    private MedicineDaoSQLImpl()
+    {
         super("Medicine");
     }
 
     /**
      * @author Semina Muratovic
      * @return MedicineDaoSQLImpl
-     * We don't need more than one object for CRUD operations on table 'quotes' so we will use Singleton
+     * We don't need more than one object for CRUD operations on table 'Medicine' so we will use Singleton
      * This method will call private constructor in instance==null and then return that instance
      */
-    public static MedicineDaoSQLImpl getInstance(){
+    public static MedicineDaoSQLImpl getInstance()
+    {
         if(instance==null)
             instance = new MedicineDaoSQLImpl();
         return instance;
     }
 
-    public static void removeInstance(){
+    public static void removeInstance()
+    {
         if(instance!=null)
             instance=null;
     }
 
     @Override
-    public Medicine row2object(ResultSet rs) throws MedicineException{
+    public Medicine row2object(ResultSet rs) throws MedicineException
+    {
         try {
             Medicine q = new Medicine();
             q.setId(rs.getInt("id"));
@@ -59,7 +62,8 @@ public class MedicineDaoSQLImpl extends AbstractDao<Medicine> implements Medicin
      * @return map representation of object
      */
     @Override
-    public Map<String, Object> object2row(Medicine object) {
+    public Map<String, Object> object2row(Medicine object)
+    {
         Map<String, Object> item = new TreeMap<>();
         item.put("id", object.getId());
         item.put("name", object.getName());
@@ -76,7 +80,8 @@ public class MedicineDaoSQLImpl extends AbstractDao<Medicine> implements Medicin
      */
 
     @Override
-    public List<Medicine> searchByText(String text) throws MedicineException{
+    public List<Medicine> searchByText(String text) throws MedicineException
+    {
         return executeQuery("SELECT * FROM Medicine WHERE name LIKE concat('%', ?, '%')", new Object[]{text});
     }
 
@@ -86,7 +91,8 @@ public class MedicineDaoSQLImpl extends AbstractDao<Medicine> implements Medicin
      * @author Semina Muratovic
      */
     @Override
-    public List<Medicine> searchByCategory(Category category) throws MedicineException{
+    public List<Medicine> searchByCategory(Category category) throws MedicineException
+    {
         return executeQuery("SELECT * FROM Medicine WHERE id = ?", new Object[]{category.getId()});
     }
 
