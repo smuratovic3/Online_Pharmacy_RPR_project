b9a21202e449044e84f1f8374c3cec03bd4a978d
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-12 17:15:31 +0100

    Formatting CategoryDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/CategoryDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CategoryDaoSQLImpl.java
@@ -2,7 +2,6 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Category;
 import ba.unsa.etf.rpr.exceptions.MedicineException;
-
 import java.sql.*;
 import java.util.Map;
 import java.util.TreeMap;
@@ -17,19 +16,22 @@ public class CategoryDaoSQLImpl extends AbstractDao<Category> implements Categor
         super("Categories");
     }
 
-    public static CategoryDaoSQLImpl getInstance(){
+    public static CategoryDaoSQLImpl getInstance()
+    {
         if(instance==null)
             instance = new CategoryDaoSQLImpl();
         return instance;
     }
 
-    public static void removeInstance(){
+    public static void removeInstance()
+    {
         if(instance!=null)
             instance=null;
     }
 
     @Override
-    public Category row2object(ResultSet rs) throws MedicineException {
+    public Category row2object(ResultSet rs) throws MedicineException
+    {
         try {
             Category cat = new Category();
             cat.setId(rs.getInt("id"));
@@ -41,7 +43,8 @@ public class CategoryDaoSQLImpl extends AbstractDao<Category> implements Categor
     }
 
     @Override
-    public Map<String, Object> object2row(Category object) {
+    public Map<String, Object> object2row(Category object)
+    {
         Map<String, Object> row = new TreeMap<>();
         row.put("id", object.getId());
         row.put("name", object.getName());
