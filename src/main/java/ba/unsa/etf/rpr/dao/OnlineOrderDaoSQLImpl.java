1de1e44a166e751a6bf5f61e059b6a4d170717b6
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-12 17:19:21 +0100

    Formatting OnlineOrderDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/OnlineOrderDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OnlineOrderDaoSQLImpl.java
@@ -2,35 +2,38 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Medicine;
 import ba.unsa.etf.rpr.domain.OnlineOrder;
 import ba.unsa.etf.rpr.exceptions.MedicineException;
-
 import java.sql.*;
 import java.util.*;
 
 public class OnlineOrderDaoSQLImpl extends AbstractDao<OnlineOrder> implements OnlineOrderDao {
     private static OnlineOrderDaoSQLImpl instance = null;
-    private OnlineOrderDaoSQLImpl() {
+    private OnlineOrderDaoSQLImpl()
+    {
         super("OnlineOrder");
     }
 
     /**
      * @author Semina Muratovic
      * @return OnlineOrderDaoSQLImpl
-     * We don't need more than one object for CRUD operations on table 'quotes' so we will use Singleton
+     * We don't need more than one object for CRUD operations on table 'OnlineOrder' so we will use Singleton
      * This method will call private constructor in instance==null and then return that instance
      */
-    public static OnlineOrderDaoSQLImpl getInstance(){
+    public static OnlineOrderDaoSQLImpl getInstance()
+    {
         if(instance==null)
             instance = new OnlineOrderDaoSQLImpl();
         return instance;
     }
 
-    public static void removeInstance(){
+    public static void removeInstance()
+    {
         if(instance!=null)
             instance=null;
     }
 
     @Override
-    public OnlineOrder row2object(ResultSet rs) throws MedicineException {
+    public OnlineOrder row2object(ResultSet rs) throws MedicineException
+    {
         try {
             OnlineOrder q = new OnlineOrder();
             q.setId(rs.getInt("id"));
@@ -48,7 +51,8 @@ public class OnlineOrderDaoSQLImpl extends AbstractDao<OnlineOrder> implements O
      * @return map representation of object
      */
     @Override
-    public Map<String, Object> object2row(OnlineOrder object) {
+    public Map<String, Object> object2row(OnlineOrder object)
+    {
         Map<String, Object> item = new TreeMap<>();
         item.put("id", object.getId());
         item.put("payment", object.getPayment());
