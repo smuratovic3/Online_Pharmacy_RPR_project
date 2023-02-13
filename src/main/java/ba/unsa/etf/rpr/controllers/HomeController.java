c5d5df446a8105276167b6c888ad85bf44df4454
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-13 17:12:46 +0100

    successfully adding new user in database

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,55 +1,21 @@
 package ba.unsa.etf.rpr.controllers;
 
-import ba.unsa.etf.rpr.business.UserManager;
-import ba.unsa.etf.rpr.domain.User;
-import ba.unsa.etf.rpr.exceptions.MedicineException;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
 import java.io.IOException;
 import java.util.Objects;
-
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class HomeController {
 
-
-
-    public TextField emailField;
-    public PasswordField passwordField;
-    @FXML
-    public void initialize(){
-       /*emailField.getStyleClass().add("poljeNijeIspravno");
-        emailField.textProperty().addListener(new ChangeListener<String>()
-        {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                if(emailField.getText().trim().isEmpty()){
-                    emailField.getStyleClass().removeAll("poljeJeIspravno");
-                    emailField.getStyleClass().add("poljeNijeIspravno");
-                }else{
-                    emailField.getStyleClass().removeAll("poljeNijeIspravno");
-                    emailField.getStyleClass().add("poljeJeIspravno");
-                }
-            }
-        });*/
-    }
     public void actionLoginWindow(ActionEvent actionEvent) throws IOException
     {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
         stage.setTitle("Log in");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
         stage.setResizable(false);
@@ -59,7 +25,7 @@ public class HomeController {
     public void actionRegisterWindow(ActionEvent actionEvent) throws IOException
     {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/registration.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/registration.fxml")));
         stage.setTitle("Registration");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
         stage.setResizable(false);
@@ -69,7 +35,7 @@ public class HomeController {
     public void actionAboutUs(ActionEvent actionEvent) throws IOException
     {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/aboutus.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/aboutus.fxml")));
         stage.setTitle("About us");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
         stage.setResizable(false);
@@ -80,7 +46,7 @@ public class HomeController {
     public void helpAction(ActionEvent actionEvent) throws IOException
     {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/help.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/help.fxml")));
         stage.setTitle("Help");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
         stage.setResizable(false);
--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -12,11 +12,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 import java.util.Objects;
+import java.util.Properties;
+import java.util.regex.Pattern;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
@@ -24,49 +31,75 @@ public class RegistrationController {
     public Button btnCancel;
     public Label messageLabel1;
     public TextField emailField;
-    public Label messageLabel4;
+    public PasswordField passwordField;
     public Label messageLabel2;
+
     UserManager userManager = new UserManager();
 
+    /**
+     Initializes the form by adding event listeners to the form fields.
+     */
+
+
     public void actionSubmit(ActionEvent actionEvent) throws IOException, MedicineException {
-        List<User> listaUsera = userManager.getAll();
 
-        //Establish a connection to the database
-        boolean registrationSuccessful = false;
-        User user = new User();
-
-        //Validate the input
-        if (emailField.getText().isEmpty()) {
-            //display an error message
-            messageLabel1.setText("Email cannot be empty!");
-        } else {
-            messageLabel1.setText("");
-            user.setEmail(emailField.getText());
+        //Retrieve user input form form field
+        String emailInput = emailField.getText();
+        String passwordInput = passwordField.getText();
+        boolean check = false;
 
 
-
-            //Add the user to the database
-            userManager.add(user);
-
-            //Set the registrationSuccessful flag to true
-            registrationSuccessful = true;
+        //validate the input
+        if (Objects.equals(emailField.getText(), "")) {
+            // Display an error message
+            messageLabel1.setText("Email can't be empty.");
+            check = true;
         }
+        if (Objects.equals(passwordField.getText(), "")) {
+            // Display an error message
+            messageLabel2.setText("Password can't be empty");
+            check = true;
+        }else {
+            // Use a regular expression to check the email format
+            String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+            Pattern pattern = Pattern.compile(emailRegex);
+            if (!pattern.matcher(emailInput).matches()) {
+                // Display an error message
+                messageLabel1.setText("Invalid email format.");
+                check = true;
+            }
+        }
+        if (!check) {
+            // Create a new user data object and set the instance variables
+            User user = new User();
+            user.setEmail(emailInput);
+            user.setPassword(passwordInput);
 
-        if (registrationSuccessful) {
             try {
+                FileReader reader = new FileReader("src/main/resources/database.properties");
+                Properties p = new Properties();
+                p.load(reader);
+                String s1 = p.getProperty("db.username"), s2 = p.getProperty("db.password"), s3 = p.getProperty("db.server");
+                Connection connection = DriverManager.getConnection(s3, s1, s2);
+                System.out.println(s1 + " " + s2 + " " + s3);
+
+                // Add the new user to the database
+                User insertedUser = userManager.add(user);
+
+                // Check if the user object was returned by the add User method from UserDaoSQLImpl.java
                 Stage stage = new Stage();
-                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/medicine.fxml")));
-                stage.setTitle("Medicine");
-                stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                Parent root = FXMLLoader.load(getClass().getResource("/fxml/medicine.fxml"));
+                stage.setTitle("Home page");
+                stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
                 stage.setResizable(false);
                 stage.show();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
+
+                // Close the connection to the database
+                connection.close();
+
+            } catch (Exception exc) {
+                exc.printStackTrace();
             }
-            //Display a confirmation message
-            messageLabel2.setText("You have been successfully registered!");
-        } else {
-            messageLabel4.setText("Invalid registration. Try again!");
         }
     }
 
@@ -80,6 +113,12 @@ public class RegistrationController {
     @FXML
     public void initialize()
     {
+        // Add an event listener to the name field
+        emailField.textProperty().addListener((observable, oldValue, newValue) -> {
+            // Clear the error message when the name field is changed
+            messageLabel1.setText("");
+        });
+
         emailField.getStyleClass().add("poljeNijeIspravno");
         emailField.textProperty().addListener(new ChangeListener<String>() {
             @Override
