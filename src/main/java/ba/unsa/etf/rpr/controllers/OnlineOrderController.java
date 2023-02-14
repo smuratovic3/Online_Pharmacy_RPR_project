50e19fb117f31d1d910246e089e42171a58f5e09
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-14 13:36:06 +0100

    made cancel order function

--- src/main/java/ba/unsa/etf/rpr/controllers/OnlineOrderController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/OnlineOrderController.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class OnlineOrderController {
+    public Button btnCancel;
+
+    public void confirm(ActionEvent actionEvent) {
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+        Stage stage =(Stage)btnCancel.getScene().getWindow();
+        stage.close();
+    }
 }
