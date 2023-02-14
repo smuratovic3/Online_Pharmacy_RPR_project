cb097dbcc58c22034a9e5eeb2b485c3ee2353150
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-14 13:43:51 +0100

    made new window when order is confirmed

--- src/main/java/ba/unsa/etf/rpr/controllers/OnlineOrderController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/OnlineOrderController.java
@@ -1,13 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class OnlineOrderController {
     public Button btnCancel;
 
     public void confirm(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/confirmorder.fxml")));
+            stage.setTitle("Confirm");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void cancelAction(ActionEvent actionEvent) {
