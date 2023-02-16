5ea57aacd4d69f3f23c206b61cb2a105394a714d
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-17 00:19:37 +0100

    Test7 i Test8

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.business.CategoryManager;
 import ba.unsa.etf.rpr.domain.Medicine;
+import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.MedicineException;
 import org.junit.jupiter.api.Test;
 
@@ -38,8 +39,37 @@ public class AppTest {
     }
 
 
+    //User konstruktor
+    @Test
+    public void testUserConstructor() {
+        User user = new User(1, "John", "Doe", "123 Main St", 5551234, "john.doe@example.com", "password123");
+        assertEquals(1, user.getId());
+        assertEquals("John", user.getName());
+        assertEquals("Doe", user.getSurname());
+        assertEquals("123 Main St", user.getAddress());
+        assertEquals(5551234, user.getPhone());
+        assertEquals("john.doe@example.com", user.getEmail());
+        assertEquals("password123", user.getPassword());
+    }
 
-
+    @Test
+    public void testUserSetterGetter() {
+        User user = new User();
+        user.setId(2);
+        user.setName("Jane");
+        user.setSurname("Doe");
+        user.setAddress("456 Oak St");
+        user.setPhone(5555678);
+        user.setEmail("jane.doe@example.com");
+        user.setPassword("abc123");
+        assertEquals(2, user.getId());
+        assertEquals("Jane", user.getName());
+        assertEquals("Doe", user.getSurname());
+        assertEquals("456 Oak St", user.getAddress());
+        assertEquals(5555678, user.getPhone());
+        assertEquals("jane.doe@example.com", user.getEmail());
+        assertEquals("abc123", user.getPassword());
+    }
     @Test
     public void validateCategoryName1() throws MedicineException {
         // Test for a valid category name
