cee162db870255888a922ad098ac1ff4b840a6f7
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-18 22:46:25 +0100

    made IntermediateDaoSQLImpl

---
+++ src/main/java/ba/unsa/etf/rpr/dao/IntermediateDaoSQLImpl.java
@@ -0,0 +1,80 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Category;
+import ba.unsa.etf.rpr.domain.IntermediateTable;
+import ba.unsa.etf.rpr.domain.Medicine;
+import ba.unsa.etf.rpr.exceptions.MedicineException;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * MySQL Implementation of DAO
+ * @author Semina Muratovic
+ */
+
+public class IntermediateDaoSQLImpl extends AbstractDao<IntermediateTable> implements IntermediateDao {
+
+    private static IntermediateDaoSQLImpl instance = null;
+    private IntermediateDaoSQLImpl()
+    {
+        super("Medicine");
+    }
+
+    /**
+     * @author Semina Muratovic
+     * @return MedicineDaoSQLImpl
+     * We don't need more than one object for CRUD operations on table 'Medicine' so we will use Singleton
+     * This method will call private constructor in instance==null and then return that instance
+     */
+    public static IntermediateDaoSQLImpl getInstance()
+    {
+        if(instance==null)
+            instance = new IntermediateDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance()
+    {
+        if(instance!=null)
+            instance=null;
+    }
+
+    @Override
+    public IntermediateTable row2object(ResultSet rs) throws MedicineException
+    {
+        try {
+            IntermediateTable q = new IntermediateTable();
+            q.setId(rs.getInt("id"));
+            q.setMedicine(DaoFactory.medicineDao().getById(rs.getInt("medicine_Id")));
+            q.setOrderOnline(DaoFactory.onlineOrderDao().getById(rs.getInt("onlineOrder_Id")));
+            return q;
+        } catch (Exception e) {
+            throw new MedicineException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @param object - object to be mapped
+     * @return map representation of object
+     */
+    @Override
+    public Map<String, Object> object2row(IntermediateTable object)
+    {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("medicine_Id", object.getMedicine().getId());
+        item.put("onlineOrder_Id", object.getOrderOnline().getId());
+        return item;
+    }
+
+
+
+
+
+
+
+
+
+}
