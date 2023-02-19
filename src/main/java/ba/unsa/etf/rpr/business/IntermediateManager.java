0c419c962df4f4077bd8a20e2dc103816631aa2f
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-19 22:27:35 +0100

    added getbyUser method

--- src/main/java/ba/unsa/etf/rpr/business/IntermediateManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/IntermediateManager.java
@@ -2,14 +2,22 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.IntermediateTable;
+import ba.unsa.etf.rpr.domain.Medicine;
 import ba.unsa.etf.rpr.exceptions.MedicineException;
+
+import java.util.List;
 
 public class IntermediateManager {
     public IntermediateTable add(IntermediateTable q) throws MedicineException
     {
         return DaoFactory.intermediateDao().add(q);
     }
+    public List<IntermediateTable> getAll() throws MedicineException {
+        return DaoFactory.intermediateDao().getAll();
+    }
 
-
+    public List<Medicine> getByUser(int idUser) throws MedicineException {
+        return DaoFactory.intermediateDao().getByUser(idUser);
+    }
 
 }
