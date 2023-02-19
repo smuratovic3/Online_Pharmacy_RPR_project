2074c7e5ef82851bacc5cfdbe44b4a6e1b56593d
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-19 22:50:45 +0100

    formatting OnlineOrderDao

--- src/main/java/ba/unsa/etf/rpr/dao/OnlineOrderDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OnlineOrderDao.java
@@ -1,10 +1,6 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.domain.Medicine;
 import ba.unsa.etf.rpr.domain.OnlineOrder;
-import ba.unsa.etf.rpr.exceptions.MedicineException;
-
-import java.util.List;
 
 /**
  * Dao interface for Category domain bean
