defb3e9b1ac4d9ffe6d873677eeee78d8950d297
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-19 22:27:14 +0100

    added getbyUser method

--- src/main/java/ba/unsa/etf/rpr/dao/IntermediateDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/IntermediateDao.java
@@ -1,6 +1,12 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.IntermediateTable;
+import ba.unsa.etf.rpr.domain.Medicine;
+import ba.unsa.etf.rpr.exceptions.MedicineException;
+
+import java.util.List;
 
 public interface IntermediateDao extends Dao<IntermediateTable> {
+    List<Medicine> getByUser(int idUser) throws MedicineException;
+
 }
