57f51385a490b5c265548511ebc4bd0ac3d304ef
smuratovic3 <smuratovic3@etf.unsa.ba>
2023-02-19 22:36:54 +0100

    formatting HomeController

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,14 +1,11 @@
 package ba.unsa.etf.rpr.controllers;
 
-
-import ba.unsa.etf.rpr.domain.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.Objects;
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class HomeController {
@@ -30,14 +27,6 @@ public class HomeController {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        /*Stage stage = new Stage();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
-        stage.setTitle("Log in");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
-
-         */
     }
 
     public void actionRegisterWindow(ActionEvent actionEvent) throws IOException
@@ -56,14 +45,6 @@ public class HomeController {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        /*Stage stage = new Stage();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/registration.fxml")));
-        stage.setTitle("Registration");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
-
-         */
     }
 
     public void actionAboutUs(ActionEvent actionEvent) throws IOException
@@ -82,15 +63,6 @@ public class HomeController {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        /*Stage stage = new Stage();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/aboutus.fxml")));
-        stage.setTitle("About us");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
-
-         */
-
     }
 
     public void helpAction(ActionEvent actionEvent) throws IOException
@@ -109,13 +81,5 @@ public class HomeController {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        /*Stage stage = new Stage();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/help.fxml")));
-        stage.setTitle("Help");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
-
-         */
     }
 }
